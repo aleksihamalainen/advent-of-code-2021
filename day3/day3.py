6fe941a8ec0d90fbd491848ee0b228d28df41c87
aleksihamalainen <aleksi.o.hamalainen@aalto.fi>
2021-12-08 23:35:27 +0200

    Add day 3 part 2 solution

--- day3/day3.py
+++ day3/day3.py
@@ -1,9 +1,10 @@
-input = []
+values = []
 
-lists = [[] for i in range(12)]
+lists = [[] for _ in range(12)]
 
 with open('input.txt') as f:
     for line in f:
+        values.append(line)
         for index, val in enumerate(line.strip()):
             lists[index].append(val)
 
@@ -25,3 +26,31 @@ for i in range(len(gamma_rate)):
 epsilon_rate = temp2.join(opposite)
 
 print(int(gamma_rate, 2) * int(epsilon_rate, 2))
+
+generator_ratings = values
+scrubber_ratings = values
+
+for i in range(12):
+    zeros = sum([1 for rating in generator_ratings if rating[i] == '0'])
+    ones = len(generator_ratings) - zeros
+    if len(generator_ratings) == 1:
+        break
+    if ones >= zeros:
+        generator_ratings = [rating for rating in generator_ratings if rating[i] == '1']
+    else:
+        generator_ratings = [rating for rating in generator_ratings if rating[i] == '0']
+
+for i in range(12):
+    zeros = sum([1 for rating in scrubber_ratings if rating[i] == '0'])
+    ones = len(scrubber_ratings) - zeros
+    if len(scrubber_ratings) == 1:
+        break
+    if ones < zeros:
+        scrubber_ratings = [rating for rating in scrubber_ratings if rating[i] == '1']
+    else:
+        scrubber_ratings = [rating for rating in scrubber_ratings if rating[i] == '0']
+
+generator_rating = int(generator_ratings[0], 2)
+scrubber_rating = int(scrubber_ratings[0], 2)
+
+print(generator_rating * scrubber_rating)
